86e5186cb91c3712bb723e38bd982efcd1cafc62
l00pss <mammadli.vugar@hotmail.com>
2026-01-14 21:52:38 +0400

    Disable display of last update time in Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -32,7 +32,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           editUrl: 'https://github.com/l00pss/supdino/tree/main/',
           showLastUpdateAuthor: false,
-          showLastUpdateTime: true,
+          showLastUpdateTime: false,
         },
         blog: {
           showReadingTime: true,
